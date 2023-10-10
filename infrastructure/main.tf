b7805de0645c140798f5f8aa5cdcbf5b024f7c69
Lucas Fernando Iori <lucasferiori@gmail.com>
2023-10-10 08:54:43 -0300

    configured the terraform scripts

---
+++ infrastructure/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "lucas-app-cinema-tf-states"
+    key     = "app-cinema.tfstate"
+    region  = "us-east-1"
+    encrypt = true
+  }
+}
+
+locals {
+  prefix = "${var.prefix}-${terraform.workspace}"
+  common_tags = {
+    Environment = terraform.workspace
+    Project     = var.project
+    ManageBy    = "Terraform"
+    Owner       = "Lucas Iori"
+  }
+}
